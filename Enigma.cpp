5f4d7f2bcfa5254262e628d4f4a5a3f778c04715
spike1818 <90467442+spike1818@users.noreply.github.com>
2021-12-27 13:13:13 -0500

    Update Enigma.cpp

--- Enigma.cpp
+++ Enigma.cpp
@@ -1,9 +1,6 @@
 //============================================================================
 // Name        : Enigma.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Author      : Samuel Pike
 //============================================================================
 
 #include <iostream>
